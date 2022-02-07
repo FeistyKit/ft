444356b631aa2e6798d265fe16c0ecad84eedaa0
FeistyKit <eeveebeevee33@gmail.com>
2022-02-06 20:45:58 -0500

    moving computers

---
+++ ft.hs
@@ -0,0 +1,20 @@
+module FT where
+
+import Data.Functor
+import Distribution.Compat.Directory (listDirectory)
+import Distribution.Utils.ShortText (encodeStringUtf8)
+import System.Environment
+
+dirToString :: String -> IO String
+dirToString a = unwords . map show <$> listDirectory a
+
+usage :: String
+usage = "USAGE: ./ft <dir to list>"
+
+-- TODO: Check if input is a directory
+parseArgs :: [String] -> String
+parseArgs [name] = name
+parseArgs _ = usage
+
+ft :: IO ()
+ft = getArgs >>= dirToString . head >>= putStrLn
