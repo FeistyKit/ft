dc0231e1c160c4b4f7f3aa9b080aa35d2b17df32
FeistyKit <eeveebeevee33@gmail.com>
2022-02-10 13:19:11 -0500

    list entries of directory

--- functree.hs
+++ functree.hs
@@ -1,16 +1,38 @@
 import Distribution.Compat.Directory (listDirectory)
 import System.Environment
+import System.Posix
 
 dirToString :: String -> IO String
 dirToString a = unwords . map show <$> listDirectory a
+
+data DirEntry = File String | Dir String [DirEntry] deriving (Show)
+
+entriesOfDir :: FilePath -> IO [DirEntry]
+entriesOfDir path = mapM (makeEntry . (fullPath ++)) =<< listDirectory path
+  where
+    makeEntry :: FilePath -> IO DirEntry
+    makeEntry path = do
+      status <- getFileStatus path
+      if isDirectory status
+        then do
+          entries <- entriesOfDir path
+          return $ Dir path entries
+        else return $ File path
+    fullPath :: String
+    fullPath = path ++ "/"
 
 usage :: String
 usage = "USAGE: ./ft <dir to list>"
 
 -- TODO: Check if input is a directory
-parseArgs :: [String] -> String
-parseArgs [name] = name
-parseArgs _ = usage
+argsValid :: [String] -> IO Bool
+argsValid [x] = isDirectory <$> getFileStatus x
+argsValid _ = return False
+
+handleInput :: [String] -> IO String
+handleInput a = do
+  valid <- argsValid a
+  if valid then dirToString $ head a else return usage
 
 main :: IO ()
-main = getArgs >>= dirToString . parseArgs >>= putStrLn
+main = getArgs >>= handleInput >>= putStrLn
